e0093f2e667975008c41346dbc16fa7e4d1d217e
Stephens Nunnally <stevinz@gmail.com>
2024-03-27 19:01:53 -0700

    Import library version

--- suey/imports.js
+++ suey/imports.js
@@ -2,12 +2,13 @@
 
 // "suey": "../build/suey.module.js"
 // "suey": "https://unpkg.com/suey/build/suey.module.js"
+// "suey": "https://unpkg.com/suey@x.x.x/build/suey.module.js"
 
 document.write(`
     <script type='importmap'>
     {
         "imports": {
-            "suey": "https://unpkg.com/suey/build/suey.module.js"
+            "suey": "https://unpkg.com/suey@0.1.22/build/suey.module.js"
         }
     }
     </script>
