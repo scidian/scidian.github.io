3d1ffaedae525e6ad303c2a283da86ed5db4f25c
Stephens Nunnally <stevinz@gmail.com>
2024-07-24 14:13:35 -0700

    Update suey examples

--- suey/imports.js
+++ suey/imports.js
@@ -1,13 +1,17 @@
-/***** Imports *****/
+/***** Local *****/
 
-// "suey": "https://unpkg.com/@salinity/suey/dist/suey.module.js"
-// "suey": "https://unpkg.com/@salinity/suey@x.x.x/dist/suey.module.js"
+// "suey": "../dist/suey.module.js"
+
+/***** CDN *****/
+
+// "suey": "https://unpkg.com/suey/dist/suey.module.js"
+// "suey": "https://unpkg.com/suey@x.x.x/dist/suey.module.js"
 
 document.write(`
     <script type='importmap'>
     {
         "imports": {
-            "suey": "https://unpkg.com/@salinity/suey@0.1.29/dist/suey.module.js"
+            "suey": "https://unpkg.com/suey@0.1.32/dist/suey.module.js"
         }
     }
     </script>
